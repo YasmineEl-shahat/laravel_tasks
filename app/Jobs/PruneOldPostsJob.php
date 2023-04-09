a77904474d2e2df4a3696044f29bae831c063132
YasmineEl-shahat <yasmenelshahat754@gmail.com>
2023-04-09 14:53:33 +0200

    prune old posts

---
+++ app/Jobs/PruneOldPostsJob.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use App\Models\Post;
+use Carbon\Carbon;
+
+class PruneOldPostsJob implements ShouldQueue
+{
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $twoYearsAgo = Carbon::now()->subYears(2);
+        Post::where('created_at', '<', $twoYearsAgo)->delete();
+    }
+}
